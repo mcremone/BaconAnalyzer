ade77efa0451c5c1f5fb42fb5e69cc9017d40ef2
Cristina Mantilla <criss.ms7@gmail.com>
2016-04-01 00:36:39 +0200

    changes for Matteo

--- MJSelection/src/VJetLoader.cc
+++ MJSelection/src/VJetLoader.cc
@@ -68,7 +68,7 @@ void VJetLoader::selectVJets(std::vector<TLorentzVector> &iVetoes,std::vector<TL
   int lCount = 0; 
   for  (int i0 = 0; i0 < fVJets->GetEntriesFast(); i0++) { 
     TJet *pVJet = (TJet*)((*fVJets)[i0]);
-    if(pVJet->pt        <=  200)                    continue;
+    // if(pVJet->pt        <=  200)                    continue;
     if(fabs(pVJet->eta) >=  2.4)                    continue;
     if(passVeto(pVJet->eta,pVJet->phi,dR,iVetoes))  continue;
     if(!passJetLooseSel(pVJet))                     continue;
