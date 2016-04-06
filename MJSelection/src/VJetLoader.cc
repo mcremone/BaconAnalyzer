e56bae7ff0ae7e7fee375a894830eff941521e7a
Cristina Mantilla <criss.ms7@gmail.com>
2016-04-06 19:17:45 +0200

    nvjets label

--- MJSelection/src/VJetLoader.cc
+++ MJSelection/src/VJetLoader.cc
@@ -47,15 +47,15 @@ void VJetLoader::setupTree(TTree *iTree, std::string iJetLabel) {
   fLabels.push_back("maxsubcsv");
   //fLabels.push_back("mindPhi");
   //fLabels.push_back("mindFPhi");
-  std::stringstream pSMT;   pSMT << iJetLabel << "mT";
+  std::stringstream pSMT;   pSMT << iJetLabel << "0_mT";
+  std::stringstream pSNJ;   pSNJ << iJetLabel << "0_njets";
   fTree = iTree;
-  fTree->Branch("nf15PUPPIjets"    ,&fNVJets ,"fNVJets/I");
+  fTree->Branch(pSNJ.str().c_str() ,&fNVJets ,(pSNJ.str()+"/I").c_str());
   fTree->Branch(pSMT.str().c_str() ,&fVMT    ,(pSMT.str()+"/F").c_str());
   for(int i0 = 0; i0 < fN*3.;                    i0++) {double pVar = 0; fVars.push_back(pVar);} // declare array of vars
   for(int i0 = 0; i0 < fN*(int(fLabels.size())); i0++) {double pVar = 0; fVars.push_back(pVar);} 
   setupNtuple(iJetLabel.c_str(),iTree,fN,fVars);                                                 // from MonoXUtils.cc => fN =1 *_pt,*_eta,*_phi for vjet0 (3*1=3)
   setupNtuple(iJetLabel.c_str(),iTree,fN,fVars,fN*3,fLabels);
-  fTree->Branch("topSize"          ,&ftopSize ,"ftopSize/D"); 
 }
 void VJetLoader::load(int iEvent) { 
   fVJets       ->Clear();
