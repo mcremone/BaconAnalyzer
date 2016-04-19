105e86bfa888952b14a3abc6aef1c196d69dab8f
Cristina Mantilla <criss.ms7@gmail.com>
2016-04-19 18:51:48 +0200

    Added setup for Bacon

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if test -z $CMSSW_VERSION; then
+  echo "[BaconAnalyzer] Need CMSSW project area setup!";
+  echo
+  return 0;
+fi
+
+CURRDIR=$PWD
+PATCHDIR=/afs/cern.ch/work/c/cmantill/public/Bacon/BaconProduction/CMSSW_7_6_2/src
+cd $CMSSW_BASE/src
+
+cp -r ${PATCHDIR}/CommonTools ./
+cp -r ${PATCHDIR}/RecoMET ./
+cp -r ${PATCHDIR}/ShowerDeconstruction ./
+
+echo
+echo "[BaconAnalyzer] Setup complete!"
+
+cd $CURRDIR
+
+return 1;
