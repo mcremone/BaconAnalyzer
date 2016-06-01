e27dab7ed37bfc329c3634c1e5535f56770b2572
Cristina Mantilla <criss.ms7@gmail.com>
2016-06-01 18:11:54 +0200

    Updating HLT file

--- MJSelection/include/EvtLoader.hh
+++ MJSelection/include/EvtLoader.hh
@@ -18,7 +18,7 @@ using namespace baconhep;
 class EvtLoader { 
 public:
   EvtLoader(TTree *iTree,std::string iName,
-            std::string iHLTFile="/src/BaconAna/DataFormats/data/HLTFile_25ns",
+            std::string iHLTFile="/afs/cern.ch/work/c/cmantill/public/Bacon/BaconProduction/CMSSW_7_6_2/src/BaconAna/DataFormats/data/HLTFile_25ns",
 	    std::string iPUWeight="/afs/cern.ch/user/p/pharris/pharris/public/bacon/prod/CMSSW_7_4_12_patch1/src/BaconAnalyzer/MJSelection/Json/puWeights_13TeV_25ns.root");
   ~EvtLoader(); 
   void reset();
